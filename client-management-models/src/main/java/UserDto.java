00b761b98624ab6f51bdc04fa914bccfdf97d85e
ulysse.jdv <u.devillartay@gmail.com>
2017-10-16 18:59:49 +0200

    init ClientController

---
+++ client-management-models/src/main/java/UserDto.java
@@ -0,0 +1,31 @@
+/**
+ * Created by ulysse on 16/10/2017.
+ */
+public class UserDto {
+    private String nom;
+    private String prenom;
+
+    public UserDto() {
+    }
+
+    public UserDto(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+}
---
+++ client-management-services/src/main/java/ClientController.java
@@ -0,0 +1,39 @@
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by ulysse on 16/10/2017.
+ */
+@RestController
+@RequestMapping("/clients")
+public class ClientController {
+
+    @GetMapping
+    public void list() {
+
+    }
+
+    @GetMapping("/{id}")
+    public void retrieve() {
+
+    }
+
+    @PostMapping
+    public void create() {
+
+    }
+
+    @PutMapping("/{id}")
+    public void update() {
+
+    }
+
+    @PatchMapping("/{id}")
+    public void partialUpdate() {
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void detroy() {
+
+    }
+}

b6c321aff2dd8a3fdb7eccbac4576da96f90a147
Ayda.Najjar <aida.najjar@etu.u-pec.fr>
2017-12-03 18:11:02 +0100

    création de la classe Entry Point

--- webank-automated-testing/src/main/java/WebankAutomatedTestingEntryPoint.java
+++ webank-automated-testing/src/main/java/WebankAutomatedTestingEntryPoint.java
@@ -1,4 +1,22 @@
-package fr.webank.automatedtesting;
+import cucumber.api.cli.Main;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
+/**
+ * Created by Ayda Najjar.
+ */
+
+@SpringBootApplication
 public class WebankAutomatedTestingEntryPoint {
+
+public static void main (String [] args){
+    int rc = 1;
+
+    try{
+        rc = Main.run(args, Thread.currentThread().getContextClassLoader());
+    }catch(IOException e){
+        e.printStackTrace();
+    }
+}
 }

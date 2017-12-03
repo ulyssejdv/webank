f6c5a904ab52a3656065ce76acbb5b779b88c1b8
alexandre.gengoul <alexandre.gengoul@gmail.com>
2017-12-03 20:03:15 +0100

    creation d'une interface repository lien entre service et repository

--- data-access-service/src/main/java/fr/webank/dataaccessservice/repositories/NotificationRepository.java
+++ data-access-service/src/main/java/fr/webank/dataaccessservice/repositories/NotificationRepository.java
@@ -1,9 +1,45 @@
 package fr.webank.dataaccessservice.repositories;
 
+import fr.webank.dataaccessservice.entities.Notification;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Alex
  */
 
-public class NotificationRepository {
+@Repository
+public class NotificationRepository implements INotificationRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Notification> getCustomerNotifications(long idCustomer) {
+        //Query query = entityManager.createQuery("select c from Conversation c where c.customer.id = :idCustomer");
+        //query.setParameter("idCustomer",idCustomer);
+
+        /*Query query = entityManager.createQuery("select n " +
+                "from Notification n " +
+                "where n.transaction.accountFrom.customerentity.idCustomer = :idCustomer " +
+                "or n.transaction.accountTo.customerentity.idCustomer = :idCustomer");
+        query.setParameter("idCustomer",idCustomer);
+
+        List<Notification> notificationArrayList = query.getResultList();
+
+        for (Notification n : notificationArrayList) {
+            System.out.println("amount : " + n.getTransaction().getTransactionAmount());
+        }*/
+
+        List<Notification> notificationArrayList = new ArrayList<Notification>();
+        Notification n = new Notification();
+        notificationArrayList.add(n);
+
+        return notificationArrayList;
+    }
 
 }

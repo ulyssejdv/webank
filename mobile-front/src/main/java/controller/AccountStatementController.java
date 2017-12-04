e710b914fa97061c11a92c83ecf8e8f536cca1c4
Boubacar NDIAYE <boubacarndiaye92@gmail.com>
2017-12-04 23:09:57 +0100

    squelette's of mobile-front

---
+++ mobile-front/src/main/java/controller/AccountStatementController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import lombok.extern.java.Log;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log
+@RestController
+@RequestMapping(path = "/")
+public class AccountStatementController {
+}

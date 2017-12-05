09dff25166e351eb3f7bd7467cd81c03e241364b
Boubacar NDIAYE <boubacarndiaye92@gmail.com>
2017-12-05 03:14:12 +0100

    constroller and service for mobile front handling query of customer

--- mobile-front/src/main/java/controller/AccountStatementController.java
+++ mobile-front/src/main/java/controller/AccountStatementController.java
@@ -1,11 +1,48 @@
 package controller;
 
 import lombok.extern.java.Log;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+/**
+ * Created by boubacar on 30/11/2017.
+ */
 @Log
-@RestController
-@RequestMapping(path = "/")
+@Controller
 public class AccountStatementController {
+
+    @Autowired
+    private AccountStatementService pdfService;
+
+//http://localhost:8001/mobile-front/releve/pdf/1
+
+    @GetMapping("releve/pdf/{basId}")
+    public ResponseEntity<byte[]> GetAccountStatementPDF(@PathVariable Long basId) {
+
+        byte[] contents = pdfService.getMyPDF(basId);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("application/pdf"));
+        ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(contents, headers, HttpStatus.OK);
+        return response;
+    }
+
+//route : http://localhost:8001/mobile-front/releve/infos/1
+
+    @GetMapping("releve/infos/{basId}")
+    public ResponseEntity<byte[]> GetAccountStatementJSON(@PathVariable Long basId) {
+
+        byte[] contents = pdfService.getMyJsonDocumnt(basId);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("application/JSON"));
+        ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(contents, headers, HttpStatus.OK);
+        return response;
+    }
 }

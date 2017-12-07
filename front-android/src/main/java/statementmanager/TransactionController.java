23967ef742205b18c6ff8d34010373a8f979fed5
Boubacar NDIAYE <boubacarndiaye92@gmail.com>
2017-12-07 19:04:22 +0100

    refac

--- front-android/src/main/java/statementmanager/TransactionController.java
+++ front-android/src/main/java/statementmanager/TransactionController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import statementmanager.RestManagement;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Pattern;

872b4484876bca5d6d4ebbdccdb2e310ed23b2f6
aaron.lellouche <aaron.lellouche@etu.u-pec.fr>
2018-01-14 16:08:47 +0100

    R algorithm modifications.

--- webank-webapp/src/main/resources/CustomeNbPrediction.R
+++ webank-webapp/src/main/resources/CustomeNbPrediction.R
@@ -1,17 +1,19 @@
-data = read.csv("C:/Users/Aaron/Documents/Esiag/ING3/PDS/R/Echantillon.csv")
+data = read.csv("C:/Users/Aaron/Documents/Esiag/ING3/PDS/R1/R/Echantillon.csv")
 
 View(data)
 plot(model)
 
-
-model <- lm(`x` ~ chomage+inflation+PIB, data = data)
+model <- lm(`x` ~ chomage+inflation+PIB+annee+mois, data = data)
 summary(model)$r.squared 
 
 
 #Chomage fictif + Inflation Fictive + PIB fictif
-new_data <- data.frame(chomage = c(9, 10, 8, 8.5, 7), inflation = c(0.35, 0.42, 1.25, 1.48, 1.12), PIB = c(38000, 41000, 42000, 39578, 40000))
+new_data <- data.frame(chomage = c(9, 10, 8, 8.5, 7), inflation = c(0.35, 0.42, 1.25, 1.48, 1.12), PIB = c(38000, 41000, 42000, 39578, 40000), 
+annee = c(2018, 2018, 2018, 2018, 2018), mois = c(1, 2, 3, 4, 5))
 prediction <- predict(model, new_data)
 View(prediction)
 
+confint(model, level = 0.95)
+summary(model)
 
 

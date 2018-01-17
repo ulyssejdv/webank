143dc9c04da116d22be0a808c2825504820a0381
aaron.lellouche <aaron.lellouche@etu.u-pec.fr>
2018-01-17 21:54:05 +0100

    Modifications of the prediction + add of the correlation matrice to prove that our model is good.

--- webank-webapp/src/main/resources/CustomeNbPrediction.R
+++ webank-webapp/src/main/resources/CustomeNbPrediction.R
@@ -9,11 +9,35 @@ summary(model)$r.squared
 
 #Chomage fictif + Inflation Fictive + PIB fictif
 new_data <- data.frame(chomage = c(9, 10, 8, 8.5, 7), inflation = c(0.35, 0.42, 1.25, 1.48, 1.12), PIB = c(38000, 41000, 42000, 39578, 40000), 
-annee = c(2018, 2018, 2018, 2018, 2018), mois = c(1, 2, 3, 4, 5))
+annee = c(2018, 2019, 2020, 2021, 2022), mois = c(1, 1, 1, 1, 1))
 prediction <- predict(model, new_data)
 View(prediction)
 
-confint(model, level = 0.95)
 summary(model)
+
+# Création d'une palette de couleur personnalisée basée sur le vert
+col <- colorRampPalette(c(
+                        # Valeurs positives (0;1)
+                        "#FFFFFF", "#8fff63", "#81e740", "#8ee725", "#6dc717", "#3b9626"))
+# Création de la matrice de corrélation
+r <- cor(data)
+# Chargement de la librairie
+library(corrplot)
+# Affichage de la matrice de corrélation
+corrplot(abs(r),
+         type = "upper",
+         method="number",
+         col= col(10),
+         cl.lim = c(0,1), #Echelle
+         insig = "blank",
+         tl.col="black", #Couleur des étiquettes
+         tl.srt=45, #Rotation des étiquettes de textes
+         diag = TRUE #Cacher les coefficients de corrélation sur la diagonale
+)
+
+
+
+
+
 
 

05faefa0ab7694e87f26eb3a1aa20ee22955bc71
Ayda.Najjar <aida.najjar@etu.u-pec.fr>
2018-02-07 23:52:26 +0100

    Config load-balancing

---
+++ load-balancing-soa/src/main/java/fr/webank/loadbalancingsoa/LoadBalancingConfiguration.java
@@ -0,0 +1,27 @@
+package fr.webank.loadbalancingsoa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.client.config.IClientConfig;
+import com.netflix.loadbalancer.IPing;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingUrl;
+import com.netflix.loadbalancer.AvailabilityFilteringRule;
+
+public class LoadBalancingConfiguration {
+
+  @Autowired
+  IClientConfig ribbonClientConfig;
+
+  @Bean
+  public IPing ribbonPing(IClientConfig config) {
+    return new PingUrl(false,"/data-access-service/");
+  }
+
+  @Bean
+  public IRule ribbonRule(IClientConfig config) {
+    return new AvailabilityFilteringRule();
+  }
+
+}

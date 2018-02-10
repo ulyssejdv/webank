99b016c45eeb3c65790f90d19485cbe43172a327
Ayda.Najjar <aida.najjar@etu.u-pec.fr>
2018-02-10 23:21:13 +0100

    Machine learning: quelque modification sur le controller

--- webank-webapp/src/main/java/fr/webank/webankwebapp/controllers/PredictionController.java
+++ webank-webapp/src/main/java/fr/webank/webankwebapp/controllers/PredictionController.java
@@ -2,15 +2,13 @@ package fr.webank.webankwebapp.controllers;
 
 import fr.webank.webankmodels.HistoriqueSoldeDto;
 import fr.webank.webankmodels.StockDto;
+import fr.webank.webankwebapp.entities.HistoriqueSoldeDtoHelperPage;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -62,15 +60,16 @@ public class PredictionController {
     }
 
     @RequestMapping(value="",method = {RequestMethod.GET})
-    public ModelAndView getListeClient() {
+    public ModelAndView getListeClient(@RequestParam(value = "page", defaultValue = "0", required = false) Integer page) {
 
         ModelAndView mav = new ModelAndView("prediction/listclients");
         RestTemplate restTemplate = new RestTemplate();
-        String url = baseurl + "/";
-        Page<HistoriqueSoldeDto> listClientDto = restTemplate.getForObject(url, Page.class);
+        String url = baseurl + "/clients?size=10&page=" + page;
+        HistoriqueSoldeDtoHelperPage listClientDto = restTemplate.getForObject(url, HistoriqueSoldeDtoHelperPage.class);
 
-        mav.addObject("listClientDto", listClientDto);
-
+        mav.addObject("listClientDto", listClientDto.getContent());
+        mav.addObject("totalpages", listClientDto.getTotalPages());
+        mav.addObject("currentpage", listClientDto.getNumber());
 
         return mav;
 

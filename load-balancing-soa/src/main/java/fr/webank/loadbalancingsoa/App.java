778e4a74dd57800fde123427a688b502f1521ee5
Ayda.Najjar <aida.najjar@etu.u-pec.fr>
2018-02-13 02:44:08 +0100

    Ajoute de fail over pour la partie SOA Stock exchange. Créer le docker pour le projet load-balancing-soa et l'integrer dans le projet parent

--- load-balancing-soa/src/main/java/fr/webank/loadbalancingsoa/App.java
+++ load-balancing-soa/src/main/java/fr/webank/loadbalancingsoa/App.java
@@ -6,8 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@RibbonClient(name = "webank", configuration = LoadBalancingConfiguration.class)
 public class App {
 
     public static void main(String[] args) {
